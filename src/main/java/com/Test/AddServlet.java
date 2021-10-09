22585a6aaa220f6f60f6629b4cc43dd12cf59678
abhijitkumarmishra <abhijitkumar2127464@gmail.coom>
2021-10-09 13:37:41 +0530

    second commit , session basic

--- src/main/java/com/Test/AddServlet.java
+++ src/main/java/com/Test/AddServlet.java
@@ -12,9 +12,12 @@ public class AddServlet extends HttpServlet
     	int i=Integer.parseInt(req.getParameter("num1"));
     	int j=Integer.parseInt(req.getParameter("num2"));
     	int k=i+j;
-    	PrintWriter out=res.getWriter();
-    	out.println("result is" + k);
-    	res.sendRedirect("sq?k="+k);
+    	HttpSession session=req.getSession();
+    	session.setAttribute("k", k);
+    	res.sendRedirect("sq");
+    	//PrintWriter out=res.getWriter();
+    	//out.println("result is" + k);
+    	//res.sendRedirect("sq?k="+k);
     }
 
 }
--- src/main/java/com/Test/SqServlet.java
+++ src/main/java/com/Test/SqServlet.java
@@ -3,6 +3,8 @@ package com.Test;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException; 
 import java.io.PrintWriter;
 import  javax.servlet.Servlet;
@@ -10,7 +12,10 @@ public class SqServlet extends  HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
-		int k = Integer.parseInt(req.getParameter("k"));
+		//int k = Integer.parseInt(req.getParameter("k"));
+		HttpSession session=req.getSession();	
+		int k= (int) session.getAttribute("k");
+		
 		k= k*k;
 		PrintWriter out = res.getWriter();
 		out.println("result is "+ k);
